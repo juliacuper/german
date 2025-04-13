c0921dcf8c28d8e275517931b22b3f1106e2cc50
juliacuper <100772884+juliacuper@users.noreply.github.com>
2025-04-13 16:37:47 +0300

    Update views.py

--- german/views.py
+++ german/views.py
@@ -21,6 +21,10 @@ def add_term(request):
     return render(request, "term_add.html")
 
 
+def add_example(request):
+    return render(request, "example_add.html")
+
+
 def send_term(request):
     if request.method == "POST":
         cache.clear()
@@ -43,6 +47,29 @@ def send_term(request):
         return render(request, "term_request.html", context)
     else:
         add_term(request)
+
+def send_example(request):
+    if request.method == "POST":
+        cache.clear()
+        user_name = request.POST.get("name")
+        new_example = request.POST.get("new_example", "")
+        new_definition = request.POST.get("new_definition", "").replace(";", ",")
+        context = {"user": user_name}
+        if len(new_definition) == 0:
+            context["success"] = False
+            context["comment"] = "Описание должно быть не пустым"
+        elif len(new_example) == 0:
+            context["success"] = False
+            context["comment"] = "Предложение должно быть не пустым"
+        else:
+            context["success"] = True
+            context["comment"] = "Ваше предложение принято"
+            terms_work.write_example(new_example, new_definition)
+        if context["success"]:
+            context["success-title"] = ""
+        return render(request, "example_request.html", context)
+    else:
+        add_example(request)
 
 
 def show_stats(request):
