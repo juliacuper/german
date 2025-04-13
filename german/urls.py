36edf652f65650071e938ade57ef14bdb2a1e02d
juliacuper <100772884+juliacuper@users.noreply.github.com>
2025-04-13 16:34:35 +0300

    Update urls.py

--- german/urls.py
+++ german/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('terms-list', views.terms_list),
     path('examples-list', views.examples_list),
     path('add-term', views.add_term),
+    peath('add-example', views.add_example),
     path('send-term', views.send_term),
     path('stats', views.show_stats)
 ]
