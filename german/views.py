47409e232c949fc3ee9a2eda572a6ae7ddb20542
juliacuper <100772884+juliacuper@users.noreply.github.com>
2025-04-13 15:15:21 +0300

    Update views.py

--- german/views.py
+++ german/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.core.cache import cache
 from . import terms_work
 
@@ -23,26 +23,23 @@ def send_term(request):
         new_term = request.POST.get("new_term", "")
         new_definition = request.POST.get("new_definition", "").replace(";", ",")
         context = {"user": user_name}
-
-        if not new_definition:
+        if len(new_definition) == 0:
             context["success"] = False
             context["comment"] = "Описание должно быть не пустым"
-        elif not new_term:
+        elif len(new_term) == 0:
             context["success"] = False
             context["comment"] = "Термин должен быть не пустым"
         else:
             context["success"] = True
             context["comment"] = "Ваш термин принят"
             terms_work.write_term(new_term, new_definition)
-
         if context["success"]:
             context["success-title"] = ""
-
         return render(request, "term_request.html", context)
     else:
-        return redirect('add_term')  # предполагается, что у вас есть URL с именем 'add_term'
+        add_term(request)
 
 
 def show_stats(request):
     stats = terms_work.get_terms_stats()
-    return render(request, "stats.html", context={"stats": stats})  # добавлен контекст для stats
+    return render(request, "stats.html", stats)
