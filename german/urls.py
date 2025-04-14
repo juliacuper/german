f5804f2779e543e3d2b3c53228c3d4d1e4deba59
juliacuper <100772884+juliacuper@users.noreply.github.com>
2025-04-14 17:14:31 +0300

    Update urls.py

--- german/urls.py
+++ german/urls.py
@@ -6,16 +6,17 @@ including views for listing terms and examples, adding new entries,
 and displaying statistics.
 """
 
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('terms-list/', views.terms_list, name='terms_list'),
-    path('examples-list/', views.examples_list, name='examples_list'),
-    path('add-term/', views.add_term, name='add_term'),
-    path('add-example/', views.add_example, name='add_example'),
-    path('send-term/', views.send_term, name='send_term'),
-    path('send-example/', views.send_example, name='send_example'),
-    path('stats/', views.show_stats, name='show_stats'),
+    path('', views.index),
+    path('terms-list', views.terms_list),
+    path('examples-list', views.examples_list),
+    path('add-term', views.add_term),
+    path('add-example', views.add_example),
+    path('send-term', views.send_term),
+    path('send-example', views.send_example),
+    path('stats', views.show_stats)
 ]
