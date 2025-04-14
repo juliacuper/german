06f68e32beb283af245e81033b1b169caaa2584a
juliacuper <100772884+juliacuper@users.noreply.github.com>
2025-04-14 21:38:31 +0300

    Update settings.py

--- german/settings.py
+++ german/settings.py
@@ -10,10 +10,11 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent  # Исправлено: заменено 'file' на '__file__'
+BASE_DIR = Path(file).resolve().parent.parent
 
 dotenv_path = os.path.join(BASE_DIR, ".env")
-load_dotenv()
+load_dotenv() 
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -25,6 +26,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
@@ -69,6 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'german.wsgi.application'
 
+
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -78,6 +81,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -97,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -108,12 +113,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+  os.path.join(BASE_DIR, 'static'),
 )
 
 # Default primary key field type
