e412e2a8a83c628959222e840494b4ae756af6dc
juliacuper <100772884+juliacuper@users.noreply.github.com>
2025-04-14 17:04:09 +0300

    Update asgi.py

--- german/asgi.py
+++ german/asgi.py
@@ -1,5 +1,10 @@
-import os
+"""
+ASGI config for german project.
 
+This module contains the ASGI application used for serving the project.
+"""
+
+import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'german.settings')
